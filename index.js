d0564399fff3e304dc09cff65bc01284384249c5
Parth Singh <parth3743@gmail.com>
2024-02-20 22:02:48 +0530

    Added isAbove() func

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const age = 20;
-const name = "Parth Singh";
+const name1 = "Parth Singh";
 const dob = "03/06/2003";
 
 function sayName() {
-  console.log("My name is Parth!!");
+  console.log("My name is ", name1);
+}
+
+function isAbove18() {
+  return age >= 18;
 }
